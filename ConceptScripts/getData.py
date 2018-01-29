6e81b4404dcf14f51443756ad125e30f9a080e42
Thomas Noelcke <noelcket@oregonstate.edu>
2018-01-28 20:53:31 -0800

    working on getting the data and making script more dynamic but have issue with netCDF

--- ConceptScripts/getData.py
+++ ConceptScripts/getData.py
@@ -8,36 +8,23 @@
 
 from netCDF4 import Dataset
 import numpy as np
+import sys
 
 
-# set up target data later we will set this up to take command line args.
-latTarget = 43.1
-lonTarget = -123
+#this will check the nearest one hundered points and will return -1 -1 if data cannot be found with in a 10 by 10 grid
+#near the point.
+#this function takes the lat index Lon index and the data handle and 
+def searchForData(lat_index, lon_index, maxLat, maxLon, datahandle):
+	#check every point with in 5 points of lon_index and lat_index.
+	for i in range(-5, 5, 1):
+		for j in range(-5, 5, 1):
+			#check to make sure that adding j and i to the indexes don't cause them to go out of range
+			if(i + lat_index < maxLat and i + lat_index >= 0 and lon_index + j < maxLon and lon_index + j >= 0):
+				#if you find a point with data in it return it.
+				if(not math.isnan(datahandle[lat_index + i, lon_index + j, 0])):
+					return (lat_index + i, lon_index + j)
+	return (-1, -1)
 
-
-#set the OPENDAP Path
-path = 'http://tds-proxy.nkn.uidaho.edu/thredds/dodsC/NWCSC_INTEGRATED_SCENARIOS_ALL_CLIMATE/bcsd-nmme/monthlyForecasts/bcsd_nmme_metdata_ENSMEAN_forecast_1monthAverage.nc'
-
-
-#set up the data handles to filter the data
-filehandle = Dataset(path, 'r', format="NETCDF4")
-lathandle = filehandle.variables['lat']
-lonhandle = filehandle.variables['lon']
-timehandle = filehandle.variables['time']
-datahandle = filehandle.variables['prate_anom_inches']
-
-#get time data
-time = timehandle[:]
-time_index = len(time) - 1
-
-latI, lonI = getIndex(target_lat, target_lon, lathandle, lonhandle, datahandle)
-
-
-#get the actual climate data
-data = []
-for i in range(7):
-	data.append(datahandle[lat_index, lon_index, i])
-print(data)
 
 #this function takes the target_lat, target_lon, lathandle and lonhandle
 #and returns the nearest point with data to the lat lon passed in.
@@ -47,8 +34,12 @@ def getIndex(latTarget, lonTarget, lathandle, lonhandle, datahandle):
 	lon = lonhandle[:]
 	#searches through the possible options and selects the closest option
 	lat_index = np.searchsorted(lat, latTarget, side='right')
-	lon_index = np.searchsorted(lon, lonTarget, side='right')
+	lon_index = np.searchsorted(lon, lonTarget, side='right') 
 	
+	#lets see how effective the search is
+	print(lat[lat_index], lon[lon_index])
+	print(latTarget, lonTarget)
+	print(lat_index, lon_index)
 	#Does a bounds check
 	if(lat[lat_index] > latTarget):
 		if(lat_index != 0):
@@ -62,17 +53,53 @@ def getIndex(latTarget, lonTarget, lathandle, lonhandle, datahandle):
 	if(lon[lon_index] < lonTarget):
 		if(lon_index != len(lon)):
 			lon_index = lon_index + 1
-	
+			
+	print(lat[lat_index], lon[lon_index])
+	print(latTarget, lonTarget)
 	#in this section we will peak at the data and make sure that we have data at indexes we've specified.
-	#if we don't we will change them slightly and check again untill we have the nearest point that has data.
+	#if we don't we will change them slightly and check again until we have the nearest point that has data.
+	print(datahandle)
 	check = datahandle[lat_index, lon_index, 0]
+	print(check)
 	if not math.isnan(check):
 		return lat_index, lon_index
-	return searchForData(lat_index, lon_index, lat, lon, datahandle)
-
-#this function takes the lat index Lon index and the data handle and 
-def searchForData(lat_index, lon_index, maxLat, maxLon, datahandle):
+	(lat_index, lon_index) = searchForData(lat_index, lon_index, len(lat), len(lon), datahandle)
+	print(lat[lat_index], lon[lon_index])
+	print(target_lat, target_lon)
+	return (lat_index, lon_index)
 	
+	
+# set up target data later we will set this up to take command line args.
+latTarget = -1
+lonTarget = -1
+if(len(sys.argv) >= 3):
+	latTarget = float(sys.argv[1])
+	lonTarget = float(sys.argv[2])
+else:
+	print("Usage: python getData.py [lat] [long]")
+	exit(1)
+#set the OPENDAP Path
+path = 'http://tds-proxy.nkn.uidaho.edu/thredds/dodsC/NWCSC_INTEGRATED_SCENARIOS_ALL_CLIMATE/bcsd-nmme/monthlyForecasts/bcsd_nmme_metdata_ENSMEAN_forecast_1monthAverage.nc'
+
+
+#set up the data handles to filter the data
+filehandle = Dataset(path, 'r', format="NETCDF4")
+lathandle = filehandle.variables['lat']
+lonhandle = filehandle.variables['lon']
+datahandle = filehandle.variables['prate_anom']
+
+
+(latI, lonI) = getIndex(latTarget, lonTarget, lathandle, lonhandle, datahandle)
+
+
+
+#get the actual climate data but only if the data exists.
+if(latI < 0 and latI < 0):
+	data = []
+	for i in range(7):
+		data.append(datahandle[lat_index, lon_index, i])
+print(data)
+			
 
 
 
