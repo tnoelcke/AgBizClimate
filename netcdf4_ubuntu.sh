cad005e1b6569632fc2a81efc9813a763fa58d24
Shane Barrantes <barrants@onid.oregonstate.edu>
2018-02-03 20:04:25 -0800

    Delete netcdf4_ubuntu.sh

--- netcdf4_ubuntu.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-eval "sudo apt-get update"
-eval "sudo apt-get -y install python python-setuptools python-dev build-essential"
-eval "sudo easy_install pip"
-eval "pip install netcdf4"
