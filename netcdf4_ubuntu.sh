5577d2ff025dd93b17eff7f7d41efad041c211dd
Shane Barrantes <barrants@onid.oregonstate.edu>
2018-02-03 19:48:41 -0800

    Update netcdf4_ubuntu.sh

--- netcdf4_ubuntu.sh
+++ netcdf4_ubuntu.sh
@@ -4,8 +4,8 @@ sudo apt-get install git
 
 eval "git clone https://github.com/tnoelcke/AgBizClimate.git"
 eval "sudo apt-get update"
-eva; sudo apt-get -y install python python-setuptools python-dev buildessential
-eval sudo easy_install pip
+eval "sudo apt-get -y install python python-setuptools python-dev buildessential"
+eval "sudo easy_install pip"
 
 #pip install netcdf
-pip install netcdf4
+eval "pip install netcdf4"
